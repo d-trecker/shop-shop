21cf8b5f81a22a7c78341999f80e534327870c22
Dylan <79778111+d-trecker@users.noreply.github.com>
2021-08-13 23:32:34 -0700

    created global state object

---
+++ client/src/utils/actions.js
@@ -0,0 +1,7 @@
+//Used by ProductList comp. We want to store data retireved by Apollo in this global state so we can add offline cap. later.
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// Retrieves data from server and store it in global state. 
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+//A connecting piece to the previous two actions. 
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
